92e35f6d1d1a8e3df6cd27172b9196bbbe96b7c2
B <B@B-PC>
2016-05-30 00:19:41 +0200

    Initial commit

---
+++ src/WebApplication3/scripts/app.component.ts
@@ -0,0 +1,8 @@
+﻿import {Component} from "@angular/core";
+@Component({
+    selector: "my-app",
+    template: "<h1>{{pageTitle}}</h1>"
+})
+export class AppComponent {
+    pageTitle: string = "My First Angular 2 App";
+}
