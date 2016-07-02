f58630e9946aa1bcd03384ebd19d868e3faf2c65
B <B@B-PC>
2016-07-02 19:02:09 +0200

    InitialCommit

--- src/WebApplication3/scripts/app.component.ts
+++ src/WebApplication3/scripts/app.component.ts
@@ -1,8 +1,33 @@
-﻿import {Component} from "@angular/core";
+﻿import {Component, AfterViewInit, ContentChild} from "@angular/core";
 @Component({
     selector: "my-app",
     templateUrl: "views/app.component.html"
 })
-export class AppComponent {
-    pageTitle: string = "My First Angular 2";
+export class AppComponent implements AfterViewInit {
+    context: CanvasRenderingContext2D;
+    myCanvas: HTMLCanvasElement;
+    pixelData: ImageData;
+    canvasWidth: number;
+    canvasHeight: number;
+
+    ngAfterViewInit() {
+        this.myCanvas = document.getElementById("myCanvas") as HTMLCanvasElement;
+        this.context = this.myCanvas.getContext("2d");
+        this.canvasHeight = this.myCanvas.height;
+        this.canvasWidth = this.myCanvas.width;
+
+        this.pixelData = this.context.createImageData(this.canvasWidth, this.canvasHeight);
+        this.tick();
+    }
+
+    tick() {
+        requestAnimationFrame(() => {
+            this.tick()
+        });
+
+        var ctx = this.context;
+        ctx.clearRect(0, 0, 400, 400);
+        ctx.fillStyle = 'red';
+        ctx.fillRect(0, 0, 111, 222);
+    }
 }
